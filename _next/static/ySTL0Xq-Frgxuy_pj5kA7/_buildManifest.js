d8e59d6d9ff13cf5d994627d0af523e6cb498aae
nicoleng <nicoleng123@gmail.com>
2022-09-05 05:59:31 +0000

    Deploying to gh-pages from @ nicoleng/home@c54a3381df5573c3fc38940cf80c4edd64fa78fc 🚀

---
+++ _next/static/ySTL0Xq-Frgxuy_pj5kA7/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-e95dc4aa21f7ed9d.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],"/about-me":["static/chunks/pages/about-me-4b352b4a0ffa1242.js"],sortedPages:["/","/_app","/_error","/about-me"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
